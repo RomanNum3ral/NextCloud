a4309ab80e775d67e54d19b7b8cf841894600378
RomanNum3ral <116513200+RomanNum3ral@users.noreply.github.com>
2024-07-23 09:38:52 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,8 +19,8 @@ sudo docker run \
 --sig-proxy=false \
 --name nextcloud-aio-mastercontainer \
 --restart always \
---publish 81:80 \
---publish 8081:8080 \
+--publish 80:80 \
+--publish 8080:8080 \
 --publish 8443:8443 \
 --volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
 --volume /var/run/docker.sock:/var/run/docker.sock:ro \
